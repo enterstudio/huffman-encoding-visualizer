7f6d86ff7298ac1af5edae2facdface4cb69dca7
Wes Cossick <WesCossick@users.noreply.github.com>
2015-04-30 14:17:34 -0500

    Use test folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp = require('gulp'),
 	mocha = require('gulp-mocha');
 
 var paths = {
-	scripts: ['./js/*.js', './tests/*.js', '!./gulpfile.js']
+	scripts: ['./js/*.js', './test/*.js', '!./gulpfile.js']
 };
 
 gulp.task('lint', function() {
@@ -16,7 +16,7 @@ gulp.task('lint', function() {
 });
 
 gulp.task('test', function() {
-	return gulp.src('./tests/*.js')
+	return gulp.src('./test/*.js')
 		.pipe(mocha({reporter: 'dot'}));
 });
 
